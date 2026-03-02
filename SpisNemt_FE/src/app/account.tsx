c5c8db9662c5244aaf85c29ca0e74c7faf421838
ramsus1311 <202107920@post.au.dk>
2026-03-02 10:50:23 +0100

    began toggleCard and began CardTheme for selected preference/allergy

--- SpisNemt_FE/src/app/account.tsx
+++ SpisNemt_FE/src/app/account.tsx
@@ -1,15 +1,20 @@
 import { View, Text } from "react-native";
+import Container from "../components/structural/Container";
+import Title from "../components/typograghy/Title";
+import { Scrollable } from "../components/structural/Scrollable";
+import ToggleCard from "../components/cards/ToggleCard";
 
 export default function Account() {
     return (
-        <View
-            style={{
-                flex: 1,
-                justifyContent: "center",
-                alignItems: "center",
-            }}
-        >
-            <Text style={{ fontSize: 24, marginBottom: 20 }}>Account page</Text>
-        </View>
-    );
+        <>
+          <Container>
+            <Title>Account</Title>
+            {/* <button title="Edit Preferences" onPress={(onPressEditPreferences) => {}} /> */}
+            <Scrollable horizontal>
+                <ToggleCard />
+            </Scrollable>
+          </Container>
+        </>
+    
+      );
 }
