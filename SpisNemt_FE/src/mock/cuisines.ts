b5c3c5534b7294d8d9f80b096ad9946b8d84baac
fred321f <frederick01nielsen@gmail.com>
2026-03-07 18:41:34 +0100

    Add mock cuisines and recipes data

---
+++ SpisNemt_FE/src/mock/cuisines.ts
@@ -0,0 +1,24 @@
+export const cuisines = [
+    {
+        title: "Italian"
+    },
+    {
+        title: "Indian"
+    },
+    {
+        title: "Mexican"
+    },
+    {
+        title: "Chinese"
+    },
+    {
+        title: "Salad"
+    },
+    {
+        title: "Dessert"
+    },
+    {
+        title: "Japanese"
+    }
+
+];
