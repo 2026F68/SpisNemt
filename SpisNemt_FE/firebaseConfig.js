0ac6ffd7bcd2b079cab9a5b7465d8b0493b379ed
fred321f <frederick01nielsen@gmail.com>
2026-03-10 13:45:31 +0100

    add env variables

--- SpisNemt_FE/firebaseConfig.js
+++ SpisNemt_FE/firebaseConfig.js
@@ -10,13 +10,13 @@ import { getFirestore } from "firebase/firestore";
 
 // Initialize Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyDS3aIQQwv-mLZXW4q0QGFlIPiPbEiLmzI",
-  authDomain: "spisnemt.firebaseapp.com",
-  projectId: "spisnemt",
-  storageBucket: "spisnemt.firebasestorage.app",
-  messagingSenderId: "353178499033",
-  appId: "1:353178499033:web:ffca92b7198b09fd2fcbc8",
-  measurementId: "G-EF35VFNEBJ"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
