dcc245f57ba540a20b2bf4656806b403d9365e0e
ramsus1311 <202107920@post.au.dk>
2026-03-16 08:48:37 +0100

    Made recommended recipes 10 random meals + API key environment variable

--- SpisNemt_FE/src/app/services/mealDbAPI/get10RandomMeals.ts
+++ SpisNemt_FE/src/app/services/mealDbAPI/get10RandomMeals.ts
@@ -1,15 +1,19 @@
+const API_KEY = process.env.EXPO_PUBLIC_MEALDB_API_KEY;
+
 export const get10RandomMeals = async () => {
   try {
-    const response = await fetch(`https://www.themealdb.com/api/json/v1/1/randomselection.php`);
-    
+    const response = await fetch(
+      `https://www.themealdb.com/api/json/v2/${API_KEY}/randomselection.php`,
+    );
+
     if (!response.ok) {
       throw new Error(`HTTP error! status: ${response.status}`);
     }
-    
+
     const data = await response.json();
-    return data.meals; 
+    return data.meals;
   } catch (error) {
     console.error("Error fetching from MealDB:", error);
-    throw error; 
+    throw error;
   }
 };
